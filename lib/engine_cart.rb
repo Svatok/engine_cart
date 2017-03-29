cfd18604d3c99a6084fdd805d056bf4f2141e162
Svatok <s_s_s@ua.fm>
2017-03-29 16:13:39 +0300

    Update engine_cart.rb

--- lib/engine_cart.rb
+++ lib/engine_cart.rb
@@ -1,4 +1,10 @@
 require "engine_cart/engine"
+require 'jquery-rails'
+require 'turbolinks', '~> 5'
+require 'jbuilder', '~> 2.5'
+require 'draper', github: 'audionerd/draper', branch: 'rails5'
+require 'rectify'
+require 'kaminari'
 
 module EngineCart
   # Your code goes here...
