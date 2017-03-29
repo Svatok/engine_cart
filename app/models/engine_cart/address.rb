cef8d2da7d432d1708ee4259a20c9e05fd3aa564
Svatok <s_s_s@ua.fm>
2017-03-29 15:00:07 +0300

    Update address.rb

--- app/models/engine_cart/address.rb
+++ app/models/engine_cart/address.rb
@@ -1,4 +1,9 @@
 module EngineCart
   class Address < ApplicationRecord
+    belongs_to :addressable, polymorphic: true
+    belongs_to :country
+    scope :billing, -> { where("address_type = 'billing'").limit(1) }
+    scope :shipping, -> { where("address_type = 'shipping'").limit(1) }
+
   end
 end
