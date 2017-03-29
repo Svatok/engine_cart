1f779b774ee653297e53e6942156d3b6bc449887
Svatok <s_s_s@ua.fm>
2017-03-29 12:20:58 +0300

    Create set_confirm.rb

---
+++ app/commands/engine_cart/set_confirm.rb
@@ -0,0 +1,25 @@
+require_dependency "engine_cart/application_controller"
+
+module EngineCart
+  class SetConfirm < Rectify::Command
+    def initialize(options)
+      @object = options[:object]
+    end
+
+    def call
+      return broadcast(:invalid, @object) unless place_order
+      OrderMailer.order_complete(@object, current_user).deliver
+      broadcast(:ok, @object)
+    end
+
+    private
+
+    def place_order
+      @object.update_attributes(order_number: new_order_number, placed_date: Date.today)
+    end
+
+    def new_order_number
+      "R%.8d" % @object.id
+    end
+  end
+end
