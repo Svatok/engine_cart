4bcf817cb976836ae71ec1d3ecaa54bc7defe305
Svatok <s_s_s@ua.fm>
2017-03-30 00:21:19 +0300

    add current_order method

--- app/controllers/engine_cart/application_controller.rb
+++ app/controllers/engine_cart/application_controller.rb
@@ -1,5 +1,10 @@
 module EngineCart
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    def current_order
+      order = Order.find_by(id: session[:order_id]) unless session[:order_id].nil?
+      order ||= Order.new
+    end
   end
 end
