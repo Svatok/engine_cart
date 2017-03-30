c6cd98b6196d5708cd72033fc3a28e5835e55399
Svatok <s_s_s@ua.fm>
2017-03-31 00:20:51 +0300

    Update application_helper.rb

--- app/helpers/engine_cart/application_helper.rb
+++ app/helpers/engine_cart/application_helper.rb
@@ -1,17 +1,17 @@
 module EngineCart
   module ApplicationHelper
     def method_missing(method, *args, &block)
-#        return super unless path_link?(method)
+       return super unless path_link?(method) && main_app.respond_to?(method)
 #        return super unless main_app.respond_to?(method) || engine_cart.respond_to?(method)
-#        main_app.send(method, *args) if main_app.respond_to?(method)
+       main_app.send(method, *args)
 #        engine_cart.send(method, *args) if engine_cart.respond_to?(method)      
-      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
-        main_app.send(method, *args)
-      elsif (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && engine_cart.respond_to?(method)
-        self.send(method, *args)
-      else
-        super
-      end
+#       if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
+#         main_app.send(method, *args)
+#       elsif (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && engine_cart.respond_to?(method)
+#         self.send(method, *args)
+#       else
+#         super
+#       end
     end
     
     def path_link?(method)
