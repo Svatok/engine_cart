25e3bc0d80657556a2be1785d4d0a492dc7ef1b4
Svatok <s_s_s@ua.fm>
2017-03-31 00:14:34 +0300

    Update application_helper.rb

--- app/helpers/engine_cart/application_helper.rb
+++ app/helpers/engine_cart/application_helper.rb
@@ -7,7 +7,7 @@ module EngineCart
 #        engine_cart.send(method, *args) if engine_cart.respond_to?(method)      
       if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
         main_app.send(method, *args)
-      elsif (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && engine_cart.respond_to?(method)
+      elsif (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && respond_to?(method)
         engine_cart.send(method, *args)
       else
         super
