239df1266b12a7a6314c027b1b9bee2363f547ef
Svatok <s_s_s@ua.fm>
2017-03-31 00:06:22 +0300

    Update engine.rb

--- lib/engine_cart/engine.rb
+++ lib/engine_cart/engine.rb
@@ -2,10 +2,5 @@ module EngineCart
   class Engine < ::Rails::Engine
     isolate_namespace EngineCart
     
-config.before_initialize do
-  ActiveSupport.on_load :action_controller do
-    helper EngineCart::ApplicationHelper
-  end
-end
   end
 end
