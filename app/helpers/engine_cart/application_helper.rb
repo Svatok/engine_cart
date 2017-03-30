dbdddff5067f0eb60bff5757d18f7eb2bfe4d3cb
Svatok <s_s_s@ua.fm>
2017-03-30 23:13:44 +0300

    Update application_helper.rb

--- app/helpers/engine_cart/application_helper.rb
+++ app/helpers/engine_cart/application_helper.rb
@@ -1,11 +1,19 @@
 module EngineCart
   module ApplicationHelper
     def method_missing(method, *args, &block)
-      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
-        main_app.send(method, *args)
-      else
-        super
-      end
+       return super unless path_link?(method)
+       return super unless main_app.respond_to?(method) || engine_cart.respond_to?(method)
+       main_app.send(method, *args) if main_app.respond_to?(method)
+       engine_cart.send(method, *args) if engine_cart.respond_to?(method)      
+#       if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
+#         main_app.send(method, *args)
+#       else
+#         super
+#       end
+    end
+    
+    def path_link?(method)
+      method.to_s.end_with?('_path') || method.to_s.end_with?('_url')
     end
   end
 end
