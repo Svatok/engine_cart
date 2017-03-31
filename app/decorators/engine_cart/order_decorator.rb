4f20650f3612511efe72ad02e38b37fd2e783f2d
Svatok <s_s_s@ua.fm>
2017-03-31 16:44:58 +0300

    Update order_decorator.rb

--- app/decorators/engine_cart/order_decorator.rb
+++ app/decorators/engine_cart/order_decorator.rb
@@ -12,5 +12,10 @@ module EngineCart
       object.total_price ||= 0
       object.total_price - object.coupon_sum - object.shipping_cost
     end
+    
+    def full_name
+      return '' unless object.addresses.present?
+      object.addresses.first.first_name + ' ' + object.addresses.first.last_name
+    end
   end
 end
