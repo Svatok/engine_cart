91256a7f2e618906b4afdf9c3e667b4533142bf7
Svatok <s_s_s@ua.fm>
2017-03-31 09:13:38 +0300

    Update application_helper.rb

--- app/helpers/engine_cart/application_helper.rb
+++ app/helpers/engine_cart/application_helper.rb
@@ -4,5 +4,10 @@ module EngineCart
        return super unless (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
        main_app.send(method, *args)
     end
+    
+    def current_order
+      order = Order.find_by(id: session[:order_id]) unless session[:order_id].nil?
+      order ||= Order.new
+    end
   end
 end
