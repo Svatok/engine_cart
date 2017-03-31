2bcbeacd044b1a02af3cc2385ff9440b47c141a6
Svatok <s_s_s@ua.fm>
2017-03-31 16:58:37 +0300

    Update orders_controller.rb

--- app/controllers/engine_cart/orders_controller.rb
+++ app/controllers/engine_cart/orders_controller.rb
@@ -1,27 +1,27 @@
 require_dependency "engine_cart/application_controller"
 
 module EngineCart
-  class OrdersController < ApplicationController
-    include Rectify::ControllerHelpers
-    helper OrdersHelper
+#   class OrdersController < ApplicationController
+#     include Rectify::ControllerHelpers
+#     helper OrdersHelper
 
-    before_action :authenticate_user!, only: [:index, :show]
+#     before_action :authenticate_user!, only: [:index, :show]
 
-    def index
-      @all_sort_params = OrdersHelper::SORTING
-      @params = sort_params
-      @orders = SorteredOrders.new(current_user.orders, params)
-    end
+#     def index
+#       @all_sort_params = OrdersHelper::SORTING
+#       @params = sort_params
+#       @orders = SorteredOrders.new(current_user.orders, params)
+#     end
 
-    def show
-      @order = Order.find_by_id(params[:id]).decorate
-      present FullOrderPresenter.new(object: @order)
-    end
+#     def show
+#       @order = Order.find_by_id(params[:id]).decorate
+#       present FullOrderPresenter.new(object: @order)
+#     end
 
-    private
+#     private
 
-    def sort_params
-      params.permit(:sort)
-    end
-  end
+#     def sort_params
+#       params.permit(:sort)
+#     end
+#   end
 end
