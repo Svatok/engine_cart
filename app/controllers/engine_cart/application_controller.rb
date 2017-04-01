44805947282404f6eca450b79eaf3231a39e9ec6
Svatok <s_s_s@ua.fm>
2017-04-02 01:20:15 +0300

    Update application_controller.rb

--- app/controllers/engine_cart/application_controller.rb
+++ app/controllers/engine_cart/application_controller.rb
@@ -5,9 +5,5 @@ module EngineCart
     protect_from_forgery with: :exception
     include ApplicationHelper
 
-#     def current_order
-#       order = Order.find_by(id: session[:order_id]) unless session[:order_id].nil?
-#       order ||= Order.new
-#     end
   end
 end
