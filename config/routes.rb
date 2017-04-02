38055a5934a69d8ff65ad9b71cc74adc40ac9c35
Svatok <s_s_s@ua.fm>
2017-04-02 20:03:41 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 EngineCart::Engine.routes.draw do
   resource :cart, only: [:show, :update], path_names: { show: '' }, path: '/'
-  resource :checkouts, only: [:show, :update]
+  resource :checkout, only: [:show, :update]
   resources :engine_orders, :controller=>"orders", only: [:index, :show]
-#   resources :orders, only: [:index, :show], path: 'engine_orders'
   root to: 'carts#show'
 end
