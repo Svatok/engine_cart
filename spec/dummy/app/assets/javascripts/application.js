d5daae90f40204d35903bb8ba36789388999b152
Svatok <s_s_s@ua.fm>
2017-04-07 16:16:39 +0300

    Update application.js

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -10,4 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require engine_cart
+//= require bootstrap-sprockets
 //= require_tree .
