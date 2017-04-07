5cd4ca04a5480af505b38a8663a6426a8ab451fc
Svatok <s_s_s@ua.fm>
2017-04-07 16:24:56 +0300

    Update routes.rb

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   mount EngineCart::Engine => "/engine_cart"
+  get '/orders', to: 'engine_cart/orders#index', as: :orders
+  get '/orders/:id', to: 'engine_cart/orders#show', as: :order
 end
