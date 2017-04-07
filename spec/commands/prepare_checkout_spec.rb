ae004e3d5fb9f38b443d62c8c46dc970730ce4b2
Svatok <s_s_s@ua.fm>
2017-04-07 17:58:26 +0300

    Update prepare_checkout_spec.rb

--- spec/commands/prepare_checkout_spec.rb
+++ spec/commands/prepare_checkout_spec.rb
@@ -1,4 +1,21 @@
 module EngineCart
-  describe PrepareCheckout do
+  describe EngineCart::PrepareCheckout do
+    let(:user) { create :user }
+    let(:order) { create :order, :with_items }
+    
+    describe 'prepare address checkout' do
+      let(:params) {  { delivery_id: delivery.id } }
+      subject { PrepareCheckout.new(order: order, params: params) }
+
+    end
+    
+    describe 'prepare shipping checkout' do
+    end    
+    
+    describe 'prepare payment checkout' do
+    end
+    
+    describe 'prepare confirm and complete checkout' do
+    end   
   end
 end
